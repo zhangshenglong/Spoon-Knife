83beec173e185fc3f472f1c91f7ee101159d30b5
zhang shenglong <zsl1988_0809@163.com>
2014-07-31 16:32:06 +0800

    change

--- bin/example5.py
+++ bin/example5.py
@@ -6,13 +6,12 @@ import os
 ipLists = []
 
 def ipMatch(line):
-	ipObject=re.search(r"(\d{1,3}\.){3}\d{1,3}",line)
-	return ipObject.group(0)
+        ipObject=re.search(r"(\d{1,3}\.){3}\d{1,3}",line)
+        return ipObject.group(0)
 
 with open('../data/e5') as file:
-    lines = file.readlines()
-    for oneLine in lines:
-        ipLists.append(ipMatch(oneLine))
+    for line in  file:
+            ipLists.append(ipMatch(line))
 
 ipLists = set(ipLists)
 ipLists = list(ipLists)
