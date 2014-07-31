080956928221e477f83047137e03c5298948ad3c
zhang shenglong <zsl1988_0809@163.com>
2014-07-31 16:01:27 +0800

    guifan

--- bin/example5.py
+++ bin/example5.py
@@ -1,14 +1,19 @@
 #!/bin/env python
 # -*- coding utf-8 -*-
 import re
-lis = []
-def matc(self):
-	result=re.search(r"(\d{1,3}\.){3}\d{1,3}",self)
-	return result.group(0)
-with open('../data/e5') as tmp:
-    lines = tmp.readlines()
-    for l in lines:
-        lis.append(matc(l))
-lis=set(lis)
-lis=list(lis)
-print 'the ip is : %s\nthe count is %d '  %(lis,len(lis))
+import sys
+import os
+ipLists = []
+
+def ipMatch(line):
+	ipObject=re.search(r"(\d{1,3}\.){3}\d{1,3}",line)
+	return ipObject.group(0)
+
+with open('../data/e5') as file:
+    lines = file.readlines()
+    for oneLine in lines:
+        ipLists.append(ipMatch(oneLine))
+
+ipLists = set(ipLists)
+ipLists = list(ipLists)
+print 'the ip is : %s\nthe count is %d '  %(ipLists,len(ipLists))
