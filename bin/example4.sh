96e64578d2794867fd09cdd5171baf0e516d57a9
zhang shenglong <zsl1988_0809@163.com>
2014-07-31 15:59:31 +0800

    guifan

--- bin/example4.sh
+++ bin/example4.sh
@@ -1,15 +1,15 @@
 #!bin/sh
-cd ../data
-cat e4_1 | tr '[A-Z]' '[a-z]' > tmp1
-cat e4_2 | tr '[A-Z]' '[a-z]' > tmp2
-sed -i 's/[^A-Za-z]/\n/g' tmp1
-sed -i 's/[^A-Za-z]/\n/g' tmp2
-sort -u tmp1 >tmp3
-sort -u tmp2 >tmp4
+path="../data"
+cat $path/e4_1 | tr '[A-Z]' '[a-z]' > $path/tmp1
+cat $path/e4_2 | tr '[A-Z]' '[a-z]' > $path/tmp2
+sed -i 's/[^A-Za-z]/\n/g' $path/tmp1
+sed -i 's/[^A-Za-z]/\n/g' $path/tmp2
+sort -u $path/tmp1 >$path/tmp3
+sort -u $path/tmp2 >$path/tmp4
 echo "The intersection is:" 
-comm -12 tmp3 tmp4
+comm -12 $path/tmp3 $path/tmp4
 echo "The difference set is:(in the first file)" 
-comm -23 tmp3 tmp4
+comm -23 $path/tmp3 $path/tmp4
 echo "The union set is:"
-cat tmp3 tmp4 | uniq
-rm tmp*
+cat $path/tmp3 $path/tmp4 | uniq
+rm $path/tmp*
