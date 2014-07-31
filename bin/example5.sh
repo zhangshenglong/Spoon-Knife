b57ad381cf5e802aed0495268380bf77a4d7f971
zhang shenglong <zsl1988_0809@163.com>
2014-07-31 16:01:08 +0800

    guifan

--- bin/example5.sh
+++ bin/example5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd ../data
-cat e5 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|awk -F" " '{print $1}' | sort -u > list
-num=$(cat list | wc -l)
-echo "The number of ip is $num,they are:"
-cat list
-rm list
+path="../data"
+cat $path/e5 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'|awk -F" " '{print $1}' | sort -u > $path/list
+ip_number=$(cat $path/list | wc -l)
+echo "The number of ip is $ip_number,they are:"
+cat $path/list
+rm $path/list
