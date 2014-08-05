4ad6e2eae53e2b798c05ce357901f22252312c5c
zhangshenglong <zsl1988_0809@163.com>
2014-08-05 13:21:30 +0800

    Delete example5.py

--- example5.py
+++
@@ -1,7 +0,0 @@
-lis = []
-with open('logt') as tmp:
-    lines = tmp.readlines()
-    for l in lines:
-        lis.append(l.split(' - - ')[0])
-a=set(lis)
-print 'the ip is : %s,the count is %d '  %(a,len(a))
